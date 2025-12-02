70bf4c4b02d5d057cd34db658c7df040f680cf8d
Oliwia <oliwkapietka1@wp.pl>
2025-12-02 19:34:52 +0100

    initialized java structure

---
+++ java_scripts/graph_visualization/src/main/java/com/language_proximity/App.java
@@ -0,0 +1,13 @@
+package com.language_proximity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
